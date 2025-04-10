d805bf0b0114251537e6786c8c7178a995fc7270
Serdar Arici <serdararici3@gmail.com>
2025-04-10 09:50:54 +0300

    Added new Razor Pages module with full CRUD functionality

---
+++ BooksAppWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BooksAppWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "Category Name cannot exceed 30 characters.")]
+        [DisplayName("CategoryName")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100.")]
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ BooksAppWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using BooksAppWebRazor_Temp.Data;
+using BooksAppWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BooksAppWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
