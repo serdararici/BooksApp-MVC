2eed3ae5ab8dd5563bd5197b43741aed481e3ae9
Serdar Arici <serdararici3@gmail.com>
2025-04-11 09:55:57 +0300

    Reset database and migrations: cleaned up old state and re-initialized schema

--- BooksApp.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
+++ BooksApp.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace BooksAppWeb.DataAccess.Migrations
+namespace BooksApp.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
@@ -21,7 +21,7 @@ namespace BooksAppWeb.DataAccess.Migrations
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("BooksAppWeb.Models.Category", b =>
+            modelBuilder.Entity("BooksApp.Models.Category", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -34,7 +34,8 @@ namespace BooksAppWeb.DataAccess.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                        .HasMaxLength(30)
+                        .HasColumnType("nvarchar(30)");
 
                     b.HasKey("Id");
 
