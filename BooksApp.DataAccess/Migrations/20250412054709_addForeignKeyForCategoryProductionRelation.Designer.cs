93ce036cb7e13b66ae61f990d3da4b505a3cb967
Serdar Arici <serdararici3@gmail.com>
2025-04-12 08:50:08 +0300

    Add Category foreign key to Product model and update database

--- BooksApp.DataAccess/Migrations/20250412054709_addForeignKeyForCategoryProductionRelation.Designer.cs
+++ BooksApp.DataAccess/Migrations/20250412054709_addForeignKeyForCategoryProductionRelation.Designer.cs
@@ -3,6 +3,7 @@ using BooksApp.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BooksApp.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250412054709_addForeignKeyForCategoryProductionRelation")]
+    partial class addForeignKeyForCategoryProductionRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
